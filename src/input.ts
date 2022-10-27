916dc6da3648cfac458871128cfa046f344a4bdf
unknown <wax.fabian@gmail.com>
2022-10-27 18:28:21 +0200

    updates input.ts

--- src/input.ts
+++ src/input.ts
@@ -4,43 +4,48 @@ let prompt = inquirer.createPromptModule();
 //Get Parameters for the Contract call
 
 export async function getInternalType() {
-    return new Promise((reject, resolve) => {
-        prompt({
-            message: "Input internalType: ",
-            type: "input",
-            name: "internalType"
-        }).then((answer) => {
-            resolve(answer.internalType);
-        }).catch((error) => {
-            reject(error);
-        })
-    })
+
+    const result = await prompt({
+        message: "Input internalType: ",
+        type: "input",
+        name: "internalType",
+    });
+
+    if (result) {
+        return result;
+    } else {
+        throw {}
+    }
+
 }
 
 export async function getName() {
-    return new Promise((reject, resolve) => {
-        prompt({
-            message: "Input name: ",
-            type: "input",
-            name: "name"
-        }).then((answer) => {
-            resolve(answer.name);
-        }).catch((error) => {
-            reject(error);
-        })
-    })
+
+    const result = await prompt({
+        message: "Input name: ",
+        type: "input",
+        name: "name"
+    });
+
+    if (result) {
+        return result;
+    } else {
+        throw {}
+    }
 }
 
 export async function getType() {
-    return new Promise((reject, resolve) => {
-        prompt({
-            message: "Input type: ",
-            type: "input",
-            name: "type"
-        }).then((answer) => {
-            resolve(answer.type);
-        }).catch((error) => {
-            reject(error);
-        })
-    })
+
+    const result = await prompt({
+        message: "Input type: ",
+        type: "input",
+        name: "type"
+    });
+
+    if (result) {
+        return result;
+    } else {
+        throw {}
+    }
+
 }
