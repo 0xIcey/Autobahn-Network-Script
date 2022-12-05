607059ade56ace3f159f8eb5879c342b2bb2688b
unknown <wax.fabian@gmail.com>
2022-12-06 00:23:33 +0100

    adds default values to priv_key and token_id, makes config check async as it should have been

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import web3 from "web3";
 import { ABI, ContractAddress } from "./contract.json";
-import { getTokenId } from "./input";
+import { getTokenId, holdBeforeExit } from "./input";
 import { Account } from "web3-core";
 import cron from "node-cron";
 import { checkForFirstStart } from "./util";
@@ -11,18 +11,20 @@ const AutobahnNetwork = new web3("https://autobahn-rpc.com");
 //Attemp to set up default wallet from PRIV_KEY
 let callerWallet: Account;
 
-let priv_key: string
-let token_ids: string[]
+let priv_key: string = "";
+let token_ids: string[] = [];
 
-try {
-  const config = checkForFirstStart();
-  priv_key = config.PRIVATE_KEY;
-  token_ids = config.TOKEN_IDS;
-  callerWallet = AutobahnNetwork.eth.accounts.wallet.add(priv_key);
-  console.log("Connected to wallet", callerWallet.address);
-} catch (error) {
-  console.error("Invalid Private key supplied. Please check config.json!");
-  process.exit(1);
+async function setKeyAndTokenIds() {
+  try {
+    const config = await checkForFirstStart();
+    priv_key = config.PRIVATE_KEY;
+    token_ids = config.TOKEN_IDS;
+    callerWallet = AutobahnNetwork.eth.accounts.wallet.add(priv_key);
+    console.log("Connected to wallet", callerWallet.address);
+  } catch (error) {
+    console.error("Invalid Private key supplied. Please check config.json!");
+    await holdBeforeExit(1);
+  }
 }
 
 //Set up contract
@@ -37,19 +39,28 @@ const tokensInConfig = token_ids[0] ? true : false;
 
 if (tokensInConfig) {
   cron.schedule("0 * * * *", main);
-  console.log('Detected token(s) written to config. Starting automated washing every day!')
+  console.log(
+    "Detected token(s) written to config. Starting automated washing every day!"
+  );
 }
 
 async function main() {
-  if (tokensInConfig) {
-    const allTokenIds = token_ids;
+  try {
+    await setKeyAndTokenIds();
 
-    for await (const tokenId of allTokenIds) {
+    if (tokensInConfig) {
+      const allTokenIds = token_ids;
+
+      for await (const tokenId of allTokenIds) {
+        await washCar(tokenId);
+      }
+    } else {
+      const tokenId = await getTokenId();
       await washCar(tokenId);
     }
-  } else {
-    const tokenId = await getTokenId();
-    await washCar(tokenId);
+    holdBeforeExit(0);
+  } catch {
+    holdBeforeExit(1);
   }
 }
 
@@ -62,23 +73,24 @@ async function washCar(tokenId: string) {
 
   try {
     //Estimating the gas cost for the Contract call
-    const estimatedGasCost = await estimateGasCost();
-
+    
+    //Estimating gas cost currently does not work on the proxy contract. Using static value
+    //const estimatedGasCost = await estimateGasCost();
+    
     //Using the send function because we actually trigger functionality that uses Gas and potentially alters the Contracts state
     const result = await carWashContract.methods
       .carWash(tokenId)
-      .send({ from: callerWallet.address, gas: estimatedGasCost });
-
+      .send({ from: callerWallet.address, gas: 25000 });
     //Should this step be successful, we can output a success message to our user
     console.log(
       `Car with tokenId ${tokenId} has been successfully washed! How shiny!`
     );
   } catch (error) {
     //We report the error back to the user and exit.
-    console.error(
-      `Transaction failed on tokenId ${tokenId}. Additional information will be provided below.`
-    );
     console.error(error);
+    console.error(
+      `\nTransaction failed on tokenId ${tokenId}. Additional information above.`
+    );
   }
 }
 
