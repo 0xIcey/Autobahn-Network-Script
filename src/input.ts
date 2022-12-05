dc9699126595462a4c6063c8aaf7b633ec404c6b
unknown <wax.fabian@gmail.com>
2022-12-06 00:27:12 +0100

    ran formatter and removed obsolete property

--- src/input.ts
+++ src/input.ts
@@ -1,20 +1,29 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 
 let prompt = inquirer.createPromptModule();
 //Get Parameters for the Contract call
 
 export async function getTokenId() {
+  const result = await prompt({
+    message: "Input tokenId: ",
+    type: "input",
+    name: "tokenId",
+  });
 
-    const result = await prompt({
-        message: "Input tokenId: ",
-        type: "input",
-        name: "tokenId",
-    });
+  if (result) {
+    return result.tokenId;
+  } else {
+    throw {};
+  }
+}
 
-    if (result) {
-        return result.tokenId;
-    } else {
-        throw {}
-    }
+export async function holdBeforeExit(code: number) {
+  const res = await prompt({
+    message: "Press the enter key to exit..",
+    name: "if this isnt here it does not wait",
+  });
 
+  if (res) {
+    process.exit(code);
+  }
 }
